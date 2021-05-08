5ad27882c067c1cfecbd7983748aa3462aa60083
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-05-08 16:15:31 +0300

    Update Motorcycles.txt

--- motorcycle-race/cmake-build-debug/CMakeFiles/motorcycle_race.dir/DependInfo.cmake
+++ motorcycle-race/cmake-build-debug/CMakeFiles/motorcycle_race.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/crist/OneDrive/Desktop/Github/motorcycle-race/main.c" "C:/Users/crist/OneDrive/Desktop/Github/motorcycle-race/cmake-build-debug/CMakeFiles/motorcycle_race.dir/main.c.obj"
+  "D:/Code/git/motorcycle-race/motorcycle-race/main.c" "D:/Code/git/motorcycle-race/motorcycle-race/cmake-build-debug/CMakeFiles/motorcycle_race.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
